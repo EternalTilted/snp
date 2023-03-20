c6fb779401c6c56932606ec15865f1ae8e455fc1
EternalTilted <94389581+EternalTilted@users.noreply.github.com>
2023-03-20 18:19:33 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,33 +1,53 @@
+class WrongNumberOfPlayersError(Exception):
+    pass
+class NoSuchStrategyError(Exception):
+    pass
+
+
 def rps_game_winner(list):
 
     if len(list) != 2:
-        raise Exception('WrongNumberOfPlayersError')
+        raise WrongNumberOfPlayersError("WrongNumberOfPlayersError")
     
     if not {list[0][1], list[1][1]}.issubset({'R', 'P', 'S'}):
-        raise Exception('NoSuchStrategyError')
+        raise NoSuchStrategyError("NoSuchStrategyError")
     
 
     if list[0][1] == 'R':
         if list[1][1] == 'P':
-            return 'player2' + ' ' + list[1][1]
+            return 'player2 ' + list[1][1]
         else:
-            return 'player1'+ ' ' + list[0][1]
+            return 'player1 ' + list[0][1]
         
     if list[0][1] == 'S':
         if list[1][1] == 'R':
-             return 'player2' + ' ' + list[1][1]
+             return 'player2 ' + list[1][1]
         else:
-            return 'player1'+ ' ' + list[0][1]
+            return 'player1 ' + list[0][1]
         
     if list[0][1] == 'P':
         if list[1][1] == 'S':
-            return 'player2' + ' ' + list[1][1]
+            return 'player2 ' + list[1][1]
         else:
-            return 'player1'+ ' ' + list[0][1]
+            return 'player1 ' + list[0][1]
     
+try:
+    print( rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]) )
+except Exception as e:
+    print(e)
 
-print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'A']]) )
-print(rps_game_winner([['player1', 'P'], ['player2', 'S']]) )
-print(rps_game_winner([['player1', 'P'], ['player2', 'P']]) )
+try:
+    print( rps_game_winner([['player1', 'P'], ['player2', 'A']]) )
+except Exception as e:
+    print(e)
+
+try:
+    print( rps_game_winner([['player1', 'A'], ['player2', 'P']]) )
+except Exception as e:
+    print(e)
+
+try:
+    print( rps_game_winner([['player1', 'A'], ['player2', 'A']]) )
+except Exception as e:
+    print(e)
 
